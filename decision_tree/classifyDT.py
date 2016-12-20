3250049ccff913b0125f1054f7983a33c94fac0b
rad <raphael.a.dumas@gmail.com>
2016-12-19 22:34:36 -0500

    Starting decision trees

--- decision_tree/classifyDT.py
+++ decision_tree/classifyDT.py
@@ -1,7 +1,6 @@
-def classify(features_train, labels_train):
-    
-    ### your code goes here--should return a trained decision tree classifer
+from sklearn.tree import DecisionTreeClassifier
 
-    
-    
+def classify(features_train, labels_train):
+    clf = DecisionTreeClassifier()
+    clf.fit(features_train, labels_train)
     return clf
--- decision_tree/studentMain.py
+++ decision_tree/studentMain.py
@@ -3,29 +3,31 @@
 """ lecture and example code for decision tree unit """
 
 import sys
+sys.path.append("../naive_bayes/")
 from class_vis import prettyPicture, output_image
 from prep_terrain_data import makeTerrainData
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pylab as pl
-from classifyDT import classify
+import classifyDT as dt
+from sklearn.metrics import accuracy_score
 
-features_train, labels_train, features_test, labels_test = makeTerrainData()
+terrain_train, terrain_labels_train, terrain_test, terrain_labels_test = makeTerrainData()
 
 
 
 ### the classify() function in classifyDT is where the magic
 ### happens--fill in this function in the file 'classifyDT.py'!
-clf = classify(features_train, labels_train)
+clf = dt.classify(terrain_train, terrain_labels_train)
 
 
+pred = clf.predict(terrain_test)
 
-
-
+acc = accuracy_score(pred, terrain_labels_test)
 
 
 #### grader code, do not modify below this line
 
-prettyPicture(clf, features_test, labels_test)
+prettyPicture(clf, terrain_test, terrain_labels_test)
 output_image("test.png", "png", open("test.png", "rb").read())
